7d7de43649e2c4c18e4b40416559d468002d25b0
Rodiewww7801 <rodion.gla@gmail.com>
2023-09-15 23:33:03 +0300

    add Config.h in gitignore and move h files in include folder

--- src/Config.h
+++ src/Config.h
@@ -1,4 +1,4 @@
-const char *wifiSSID = "STRONG_0ff0_2.4GHz";
-const char *wifiPassword = "FnMI6mG5Fm";
-const char *websocketIP = "192.168.1.17";
+const char *wifiSSID = "WIFI_SSID";
+const char *wifiPassword = "WIFI_PASSWORD";
+const char *websocketIP = "SERVER_IP_ADDRESS";
 const unsigned int websocketPort = 81;
