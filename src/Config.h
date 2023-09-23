544b00a1fafcba887329a997fd7874fcc552b0aa
Rodie7801 <rodion.gla@gmail.com>
2023-09-23 03:05:27 +0300

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -1,4 +1,4 @@
 const char *wifiSSID = "WIFI_SSID";
 const char *wifiPassword = "WIFI_PASSWORD";
-const char *websocketIP = "SERVER_IP_ADDRESS";
+const char *host = "HOST";
 const unsigned int websocketPort = 81;
