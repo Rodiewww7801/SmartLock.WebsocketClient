19d9a5345e047c690dd60ebcce4980b6b478b8eb
Rodie7801 <rodion.gla@gmail.com>
2023-09-24 02:38:39 +0300

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -2,4 +2,5 @@ const char *wifiSSID = "WIFI_SSID";
 const char *wifiPassword = "WIFI_PASSWORD";
 const char *host = "HOST";
 const unsigned int websocketPort = 81;
-const char *filePathToRootCACert = "/RootCACertificate.cer";
+const char *filePathToRootCACert = "FILE_PATH";
+const char *deviceSerialNumber = "DEVICE_SERIAL_NUMBER";
